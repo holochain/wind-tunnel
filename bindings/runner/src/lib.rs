2183aa4bff53be83986f74a56302af8ceef78478
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-03-05 14:50:58 +0000

    More user rust docs

--- bindings/runner/src/lib.rs
+++ bindings/runner/src/lib.rs
@@ -1,12 +1,20 @@
-/// Common operations for Holochain scenarios.
 mod common;
+
 mod context;
 mod macros;
 mod runner_context;
 
 pub mod prelude {
+    /// Common operations for Holochain scenarios.
+    ///
+    /// This is a good place to start if you are getting started writing scenarios.
     pub use crate::common::*;
+
     pub use crate::context::HolochainAgentContext;
     pub use crate::runner_context::HolochainRunnerContext;
+
+    /// Re-export of the `wind_tunnel_runner` prelude.
+    ///
+    /// This is for convenience so that you can depend on a single crate for the runner in your scenarios.
     pub use wind_tunnel_runner::prelude::*;
 }
