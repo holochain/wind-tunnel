d9213c8d847bb7176bb31c4731136d9109a32674
neonphog <neonphog@gmail.com>
2024-08-16 13:55:37 -0600

    initial working trycp version of write_validated scenario

---
+++ scenarios/trycp_write_validated/src/main.rs
@@ -0,0 +1,142 @@
+use holochain_types::prelude::ActionHash;
+use std::time::{Duration, Instant};
+use trycp_wind_tunnel_runner::embed_conductor_config;
+use trycp_wind_tunnel_runner::prelude::*;
+use validated_integrity::UpdateSampleEntryInput;
+
+embed_conductor_config!();
+
+#[derive(Debug, Default)]
+pub struct ScenarioValues {}
+
+impl UserValuesConstraint for ScenarioValues {}
+
+fn agent_setup(
+    ctx: &mut AgentContext<TryCPRunnerContext, TryCPAgentContext<ScenarioValues>>,
+) -> HookResult {
+    connect_trycp_client(ctx)?;
+    reset_trycp_remote(ctx)?;
+
+    let client = ctx.get().trycp_client();
+    let agent_name = ctx.agent_name().to_string();
+
+    ctx.runner_context()
+        .executor()
+        .execute_in_place(async move {
+            client
+                .configure_player(agent_name.clone(), conductor_config().to_string(), None)
+                .await?;
+
+            client.startup(agent_name.clone(), None).await?;
+
+            Ok(())
+        })?;
+
+    install_app(
+        ctx,
+        scenario_happ_path!("validated"),
+        &"validated".to_string(),
+    )?;
+    try_wait_for_min_peers(ctx, Duration::from_secs(120))?;
+
+    Ok(())
+}
+
+fn agent_behaviour(
+    ctx: &mut AgentContext<TryCPRunnerContext, TryCPAgentContext<ScenarioValues>>,
+) -> HookResult {
+    let client = ctx.get().trycp_client();
+
+    let app_port = ctx.get().app_port();
+    let cell_id = ctx.get().cell_id();
+    let reporter = ctx.runner_context().reporter();
+
+    ctx.runner_context()
+        .executor()
+        .execute_in_place(async move {
+            let start = Instant::now();
+
+            let action_hash: ActionHash = client
+                .call_zome(
+                    app_port,
+                    cell_id.clone(),
+                    "validated",
+                    "create_sample_entry",
+                    "this is a test entry value",
+                    Some(Duration::from_secs(80)),
+                )
+                .await
+                .map_err(|e| anyhow::anyhow!("call failure: {:?}", e))?
+                .decode()
+                .map_err(|e| anyhow::anyhow!("Decoding failure: {:?}", e))?;
+
+            reporter.add_custom(
+                ReportMetric::new("create_sample_entry_time")
+                    .with_field("value", start.elapsed().as_secs_f64()),
+            );
+
+            let start = Instant::now();
+
+            let _: ActionHash = client
+                .call_zome(
+                    app_port,
+                    cell_id,
+                    "validated",
+                    "update_sample_entry",
+                    UpdateSampleEntryInput {
+                        original: action_hash,
+                        new_value: "the old string was a bit boring".to_string(),
+                    },
+                    Some(Duration::from_secs(80)),
+                )
+                .await
+                .map_err(|e| anyhow::anyhow!("call failure: {:?}", e))?
+                .decode()
+                .map_err(|e| anyhow::anyhow!("Decoding failure: {:?}", e))?;
+
+            reporter.add_custom(
+                ReportMetric::new("update_sample_entry_time")
+                    .with_field("value", start.elapsed().as_secs_f64()),
+            );
+
+            Ok(())
+        })?;
+
+    Ok(())
+}
+
+fn agent_teardown(
+    ctx: &mut AgentContext<TryCPRunnerContext, TryCPAgentContext<ScenarioValues>>,
+) -> HookResult {
+    if let Err(e) = dump_logs(ctx) {
+        log::warn!("Failed to dump logs: {:?}", e);
+    }
+
+    // Best effort to remove data and cleanup.
+    // You should comment out this line if you want to examine the result of the scenario run!
+    let _ = reset_trycp_remote(ctx);
+
+    // Alternatively, you can just shut down the remote conductor instead of shutting it down and removing data.
+    // shutdown_remote(ctx)?;
+
+    disconnect_trycp_client(ctx)?;
+
+    Ok(())
+}
+
+fn main() -> WindTunnelResult<()> {
+    let builder = TryCPScenarioDefinitionBuilder::<
+        TryCPRunnerContext,
+        TryCPAgentContext<ScenarioValues>,
+    >::new_with_init(env!("CARGO_PKG_NAME"))?
+    .into_std()
+    .use_agent_setup(agent_setup)
+    .use_agent_behaviour(agent_behaviour)
+    .use_agent_teardown(agent_teardown);
+
+    let agents_at_completion = run(builder)?;
+
+    println!("Finished with {} agents", agents_at_completion);
+
+    Ok(())
+}
