b5501100e50a3246d49ea6ba059c05b9fee01e4d
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-09-17 14:58:44 +0300

    remove overridden holochain flake input

--- flake.nix
+++ flake.nix
@@ -12,8 +12,6 @@
         flake-parts.follows = "flake-parts";
         crane.follows = "crane";
         rust-overlay.follows = "rust-overlay";
-        # TODO: Revert when new release of holochain is made with updated hc-sandbox cli
-        holochain.url = "github:holochain/holochain?ref=develop";
       };
     };
 
