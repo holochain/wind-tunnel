e9653c93cf23b3c44139cdcceca03dd4109a3666
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-10-04 15:15:07 +0100

    Initial summarizer tool

--- summariser/src/main.rs
+++ summariser/src/main.rs
@@ -1,12 +1,15 @@
+use crate::scenario::summarize_countersigning_two_party;
 use anyhow::Context;
-use influxdb::{Query, ReadQuery};
-use itertools::Itertools;
+use futures::FutureExt;
+use scenario::summarize_app_install;
 use std::path::PathBuf;
-use chrono::{DateTime, NaiveDateTime};
-use influxdb::integrations::serde_integration::DatabaseQueryResult;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use wind_tunnel_summary_model::{load_summary_runs, RunSummary};
+use wind_tunnel_summary_model::load_summary_runs;
+
+mod filter;
+mod frame;
+mod model;
+mod query;
+mod scenario;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -15,20 +18,16 @@ async fn main() -> anyhow::Result<()> {
 
     // Note that this is just a simple selection strategy. If we have run scenarios with more than
     // one configuration, we might want to select multiple summaries per scenario name.
-    let latest_summaries = summary_runs
-        .into_iter()
-        .into_group_map_by(|summary| summary.scenario_name.clone())
-        .into_iter()
-        .map(|(_, mut summaries)| {
-            summaries.sort_by_key(|summary| summary.started_at);
+    let latest_summaries = filter::latest_run_summaries_by_name(&summary_runs);
 
-            // Safe to unwrap because there must have been at least one element
-            summaries.last().unwrap().clone()
-        })
-        .collect::<Vec<_>>();
+    let latest_by_config_summaries = filter::latest_run_summaries_by_name_and_config(summary_runs);
 
     for summary in &latest_summaries {
         println!("{:?}", summary);
+    }
+
+    for (name, fingerprint, summary) in &latest_by_config_summaries {
+        println!("{:?} {:?} {:?}", name, fingerprint, summary);
     }
 
     let client = influxdb::Client::new(
@@ -41,62 +40,41 @@ async fn main() -> anyhow::Result<()> {
         std::env::var("INFLUX_TOKEN")
             .context("Cannot read metrics without environment variable `INFLUX_TOKEN`")?,
     );
-    report_countersigning_two_party(client, latest_summaries[0].clone()).await?;
+
+    let outcome = futures::future::join_all(latest_by_config_summaries.into_iter().filter_map(
+        |(name, _, summary)| {
+            let client = client.clone();
+            match name.as_str() {
+                "app_install" => Some(
+                    async move {
+                        summarize_app_install(client.clone(), summary.clone())
+                            .await
+                            .context("App install report")
+                    }
+                    .boxed(),
+                ),
+                "two_party_countersigning" => Some(
+                    async move {
+                        summarize_countersigning_two_party(client.clone(), summary.clone())
+                            .await
+                            .context("Countersigning, two party, report")
+                    }
+                    .boxed(),
+                ),
+                _ => {
+                    println!("No report for scenario: {}", name);
+                    None
+                }
+            }
+        },
+    ))
+    .await
+    .into_iter()
+    .collect::<anyhow::Result<Vec<_>>>()?;
+    // .collect::<Vec<_>>()
+    // .await
+
+    println!("Outcome: {:?}", outcome);
 
     Ok(())
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct QueryResult {
-    statement_id: u32,
-    series: Vec<QuerySeries>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct QuerySeries {
-    name: String,
-    columns: Vec<String>,
-    values: Vec<Vec<Value>>,
-}
-
-async fn report_countersigning_two_party(client: influxdb::Client, summary: RunSummary) -> anyhow::Result<()> {
-    assert_eq!(summary.scenario_name, "two_party_countersigning");
-
-    let mean = query_mean(client.clone(), &summary, "wt.custom.countersigning_session_initiated_duration").await?;
-    let spread = query_spread(client.clone(), &summary, "wt.custom.countersigning_session_initiated_duration").await?;
-    let std_dev = query_std_dev(client.clone(), &summary, "wt.custom.countersigning_session_initiated_duration").await?;
-
-    println!("Mean: {}, Spread: {}, Std Dev: {}", mean, spread, std_dev);
-
-    Ok(())
-}
-
-async fn query_mean(client: influxdb::Client, summary: &RunSummary, metric: &str) -> anyhow::Result<f64> {
-    let q = ReadQuery::new(format!(r#"SELECT MEAN(value) FROM "windtunnel"."autogen"."{}" WHERE run_id = '{}'"#, metric, summary.run_id));
-    let res = client.json_query(q).await?;
-    read_float_result(res)
-}
-
-async fn query_spread(client: influxdb::Client, summary: &RunSummary, metric: &str) -> anyhow::Result<f64> {
-    let q = ReadQuery::new(format!(r#"SELECT SPREAD(value) FROM "windtunnel"."autogen"."{}" WHERE run_id = '{}'"#, metric, summary.run_id));
-    let res = client.json_query(q).await?;
-    read_float_result(res)
-}
-
-async fn query_std_dev(client: influxdb::Client, summary: &RunSummary, metric: &str) -> anyhow::Result<f64> {
-    let q = ReadQuery::new(format!(r#"SELECT STDDEV(value) FROM "windtunnel"."autogen"."{}" WHERE run_id = '{}'"#, metric, summary.run_id));
-    let res = client.json_query(q).await?;
-    read_float_result(res)
-}
-
-fn read_float_result(result: DatabaseQueryResult) -> anyhow::Result<f64> {
-    let value = result.results[0].clone();
-    let x = serde_json::from_value::<QueryResult>(value)?;
-    Ok(x.series[0].values[0][1].as_number().unwrap().as_f64().unwrap())
-}
-
-fn read_int_result(result: DatabaseQueryResult) -> anyhow::Result<i64> {
-    let value = result.results[0].clone();
-    let x = serde_json::from_value::<QueryResult>(value)?;
-    Ok(x.series[0].values[0][1].as_number().unwrap().as_i64().unwrap())
 }
