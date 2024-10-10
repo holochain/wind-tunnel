43cbee2bf9fd40094c9f3ca40502e626fdcf384d
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-10-10 13:54:08 +0100

    Summary for single write many read

--- summariser/src/scenario/single_write_many_read.rs
+++ summariser/src/scenario/single_write_many_read.rs
@@ -3,36 +3,32 @@ use crate::frame::LoadError;
 use crate::model::{StandardTimingsStats, SummaryOutput};
 use crate::query;
 use anyhow::Context;
+use polars::prelude::*;
 use serde::{Deserialize, Serialize};
 use wind_tunnel_summary_model::RunSummary;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct RemoteCallRateSummary {
-    dispatch: StandardTimingsStats,
-    round_trip: StandardTimingsStats,
+struct SingleWriteManyReadSummary {
+    read_call: StandardTimingsStats,
     mean_rate_10s: f64,
     error_count: usize,
 }
 
-pub(crate) async fn summarize_remote_call_rate(
+pub(crate) async fn summarize_single_write_many_read(
     client: influxdb::Client,
     summary: RunSummary,
 ) -> anyhow::Result<SummaryOutput> {
-    assert_eq!(summary.scenario_name, "remote_call_rate");
+    assert_eq!(summary.scenario_name, "single_write_many_read");
 
-    let dispatch_frame =
-        query::query_custom_data(client.clone(), &summary, "wt.custom.remote_call_dispatch")
-            .await
-            .context("Load send data")?;
-
-    let round_trip_frame =
-        query::query_custom_data(client.clone(), &summary, "wt.custom.remote_call_round_trip")
-            .await
-            .context("Load recv data")?;
-
-    let call_count = query::query_zome_call_instrument_data(client.clone(), &summary)
+    let zome_calls = query::query_zome_call_instrument_data(client.clone(), &summary)
         .await
         .context("Load zome call data")?;
+
+    let zome_calls = zome_calls
+        .clone()
+        .lazy()
+        .filter(col("fn_name").eq(lit("get_sample_entry")))
+        .collect()?;
 
     let error_count =
         match query::query_zome_call_instrument_data_errors(client.clone(), &summary).await {
@@ -47,12 +43,9 @@ pub(crate) async fn summarize_remote_call_rate(
 
     SummaryOutput::new(
         summary,
-        RemoteCallRateSummary {
-            dispatch: standard_timing_stats(dispatch_frame, "value", None)
-                .context("Send timing stats")?,
-            round_trip: standard_timing_stats(round_trip_frame, "value", None)
-                .context("Recv timing stats")?,
-            mean_rate_10s: standard_rate(call_count.clone(), "value", "10s")?,
+        SingleWriteManyReadSummary {
+            read_call: standard_timing_stats(zome_calls.clone(), "value", None)?,
+            mean_rate_10s: standard_rate(zome_calls.clone(), "value", "10s")?,
             error_count,
         },
     )
