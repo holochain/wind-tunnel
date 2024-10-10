9d6d07bd2301583022d88cfdf5ec28ca7351b089
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-10-10 18:34:11 +0100

    Summary for write validated

--- summariser/src/scenario.rs
+++ summariser/src/scenario.rs
@@ -2,12 +2,14 @@ mod app_install;
 mod first_call;
 mod local_signals;
 mod remote_call_rate;
-mod two_party_countersigning;
 mod single_write_many_read;
+mod two_party_countersigning;
+mod write_validated;
 
 pub(crate) use app_install::summarize_app_install;
 pub(crate) use first_call::summarize_first_call;
 pub(crate) use local_signals::summarize_local_signals;
 pub(crate) use remote_call_rate::summarize_remote_call_rate;
-pub(crate) use two_party_countersigning::summarize_countersigning_two_party;
 pub(crate) use single_write_many_read::summarize_single_write_many_read;
+pub(crate) use two_party_countersigning::summarize_countersigning_two_party;
+pub(crate) use write_validated::summarize_write_validated;
