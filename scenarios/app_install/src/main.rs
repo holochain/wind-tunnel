af848d6ba3519e468757fb6934b881afc8224c73
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-10-14 19:57:39 +0100

    Add test for app install large

--- scenarios/app_install/src/main.rs
+++ scenarios/app_install/src/main.rs
@@ -1,7 +1,6 @@
 use holochain_wind_tunnel_runner::prelude::*;
 use holochain_wind_tunnel_runner::scenario_happ_path;
 use std::path::PathBuf;
-use std::time::Instant;
 
 #[derive(Debug, Default)]
 struct ScenarioValues {
