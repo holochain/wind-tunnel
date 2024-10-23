3f087d8c238a8d508d6a0e6eeca8993494d35457
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-10-23 21:00:39 +0100

    Fix test data name collisions

--- summariser/tests/snapshot.rs
+++ summariser/tests/snapshot.rs
@@ -1,8 +1,9 @@
 use anyhow::Context;
 use holochain_summariser::{execute_report_for_run_summary, model::SummaryOutput};
-use std::path::Path;
+use std::io::BufRead;
+use std::path::{Path, PathBuf};
 use walkdir::{DirEntry, WalkDir};
-use wind_tunnel_summary_model::load_run_summary;
+use wind_tunnel_summary_model::{load_run_summary, load_summary_runs, RunSummary};
 
 macro_rules! run_snapshot_test {
     ($summary_fingerprint:literal) => {
@@ -37,87 +38,143 @@ macro_rules! run_snapshot_test {
     };
 }
 
+#[test]
+fn ensure_run_summary_fingerprints_accurate() {
+    for entry in WalkDir::new(
+        Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("test_data")
+            .join("1_run_summaries"),
+    )
+    .into_iter()
+    {
+        let entry = entry.unwrap();
+        if entry.file_type().is_file() {
+            let summary = load_run_summary(
+                std::fs::File::open(entry.path()).expect("Failed to load run summary"),
+            )
+            .unwrap();
+
+            summary.fingerprint();
+            std::fs::rename(
+                entry.path(),
+                entry.path().with_file_name(format!(
+                    "{}-{}.json",
+                    summary.scenario_name,
+                    summary.fingerprint()
+                )),
+            )
+            .expect("Failed to rename file");
+        }
+    }
+}
+
+#[test]
+fn ensure_summary_output_fingerprints_accurate() {
+    for entry in WalkDir::new(
+        Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("test_data")
+            .join("3_summary_outputs"),
+    )
+    .into_iter()
+    {
+        let entry = entry.unwrap();
+        if entry.file_type().is_file() {
+            let summary_output = load_summary_output(entry.path().into()).unwrap();
+
+            std::fs::rename(
+                entry.path(),
+                entry.path().with_file_name(format!(
+                    "{}-{}.json",
+                    summary_output.run_summary.scenario_name,
+                    summary_output.run_summary.fingerprint()
+                )),
+            )
+            .expect("Failed to rename file");
+        }
+    }
+}
+
 #[tokio::test]
 async fn app_install_minimal() -> anyhow::Result<()> {
-    run_snapshot_test!("f8a3fe811d284d42c923571701e31ffdf01cfeaaa11561e34d3712aedb2a95ae");
+    run_snapshot_test!("8468d686b75c702a7c523c4da615b817c09d20d5ead54f2806513b756652055a");
     Ok(())
 }
 
 #[tokio::test]
 async fn app_install_large() -> anyhow::Result<()> {
-    run_snapshot_test!("9b7bade717a222157887e8b2973cee491ffcc98c7417d37dec8fc27ce2dfe305");
+    run_snapshot_test!("52bd6609472c523e0561f09d7daecaac880d348f71241f6fabf71d28795e79ec");
     Ok(())
 }
 
 #[tokio::test]
 async fn dht_sync_lag() -> anyhow::Result<()> {
-    run_snapshot_test!("28ccc7d6a728cf63055ea210996cd57b6ebeb5793bb3d9186d6a9b9c06a98765");
+    run_snapshot_test!("0ce3084e5ea38d168b05008fc88db01f685cc4e754be264a705f54386891f955");
     Ok(())
 }
 
 #[tokio::test]
 async fn first_call() -> anyhow::Result<()> {
-    run_snapshot_test!("1c1782ff0b6a1fce9640342be79bc23970833bc535cba493f9e494e65919d436");
+    run_snapshot_test!("608026f5fcbb4e5e87cba8d616ae379c20a7c8930138d36f11ddde5134e4e730");
     Ok(())
 }
 
 #[tokio::test]
 async fn local_signals() -> anyhow::Result<()> {
-    run_snapshot_test!("1c1782ff0b6a1fce9640342be79bc23970833bc535cba493f9e494e65919d436");
+    run_snapshot_test!("1fd6a6042bf4d93742d9fe912adac0c896d133f8cccb90758db60fefd09a7060");
     Ok(())
 }
 
 #[tokio::test]
 async fn remote_call_rate() -> anyhow::Result<()> {
-    run_snapshot_test!("3ca57c491607c8639ba04caef533f49833549bf7a2fab9851ba2cb9494d16fe2");
+    run_snapshot_test!("f92e98962b23bfe104373a735dd9af8eb363e347a0c528902d4a2aaa8351cd74");
     Ok(())
 }
 
 #[tokio::test]
 async fn single_write_many_read() -> anyhow::Result<()> {
-    run_snapshot_test!("1c1782ff0b6a1fce9640342be79bc23970833bc535cba493f9e494e65919d436");
+    run_snapshot_test!("53072be05686ee83ae234f248d2791e0576cdc05065954975b89549571613a97");
     Ok(())
 }
 
 #[tokio::test]
 async fn trycp_write_validated() -> anyhow::Result<()> {
-    run_snapshot_test!("3ca57c491607c8639ba04caef533f49833549bf7a2fab9851ba2cb9494d16fe2");
+    run_snapshot_test!("67451208b842208de0861073fb576a921685acf71f0c4cef1b87340527f22b49");
     Ok(())
 }
 
 #[tokio::test]
 async fn two_party_countersigning() -> anyhow::Result<()> {
-    run_snapshot_test!("8f5f4e70e7852399484a024dcfec72909b3778c7edb642e6750aec5772bc2fc0");
+    run_snapshot_test!("3cdc5a29d42fbe93e971508e7cd8856367465e047c870b60283ed86a5bf5687c");
     Ok(())
 }
 
 #[tokio::test]
 async fn validation_receipts() -> anyhow::Result<()> {
-    run_snapshot_test!("3265009665eab80d8b796d448aa6ae1739a7b416f4f98fda7e37c9fc5d898729");
+    run_snapshot_test!("7ec48e9c40fde50ceb5fccc5cfdcaabd69eebd148eb9c3590fda2e2419152f70");
     Ok(())
 }
 
 #[tokio::test]
 async fn write_query() -> anyhow::Result<()> {
-    run_snapshot_test!("1c1782ff0b6a1fce9640342be79bc23970833bc535cba493f9e494e65919d436");
+    run_snapshot_test!("d785bb3d4bdb5e2ae9405c5e1899cb49c9a6fbe34e8baa8e6df8819dee5c7233");
     Ok(())
 }
 
 #[tokio::test]
 async fn write_read() -> anyhow::Result<()> {
-    run_snapshot_test!("1c1782ff0b6a1fce9640342be79bc23970833bc535cba493f9e494e65919d436");
+    run_snapshot_test!("2beac84ddff6d46234ece3c1227d9c19ef1d1bc0e78e4c0aac2e0e4eacf6ef1c");
     Ok(())
 }
 
 #[tokio::test]
 async fn write_validated() -> anyhow::Result<()> {
-    run_snapshot_test!("1c1782ff0b6a1fce9640342be79bc23970833bc535cba493f9e494e65919d436");
+    run_snapshot_test!("5d3ab06e123d1245b15c348d1512b7dd0f6feff44f0b0a3b79b1b72ca3e4b2b7");
     Ok(())
 }
 
 #[tokio::test]
 async fn zome_call_single_value() -> anyhow::Result<()> {
-    run_snapshot_test!("1c1782ff0b6a1fce9640342be79bc23970833bc535cba493f9e494e65919d436");
+    run_snapshot_test!("4e74c47aa5158bbdc0c010fdc44de565c8b6592e472026f0bca9464d69e1e99b");
     Ok(())
 }
 
@@ -136,4 +193,11 @@ fn find_test_data_file(summary_fingerprint: &str, stage: &str) -> Option<DirEntr
             .map(|name| name.contains(summary_fingerprint))
             .unwrap_or(false)
     })
+}
+
+/// Load summary output from a file
+pub fn load_summary_output(path: PathBuf) -> anyhow::Result<SummaryOutput> {
+    let file = std::fs::File::open(path)?;
+    let reader = std::io::BufReader::new(file);
+    serde_json::from_reader(reader).context("Failed to load summary output")
 }
