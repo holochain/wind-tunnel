a347bb60f232b20d9e6d8300dba923cc478e197e
Callum Dunster <cdunster@users.noreply.github.com>
2024-10-29 15:12:28 +0100

    docs: remove extra `///` in doc-comment

--- scenarios/scenario_build.rs
+++ scenarios/scenario_build.rs
@@ -2,7 +2,7 @@ use happ_builder::{build_happs, required_tools_available, BuildOptions, HappBuil
 
 /// Build the required DNA(s) and hApps for the scenario.
 /// The built DNA(s) will appear as `/dnas/<scenario-name>/<dna-name>.dna` from the project root.
-/// /// The built hApp(s) will appear as `/happs/<scenario-name>/<happ-name>.happ` from the project root.
+/// The built hApp(s) will appear as `/happs/<scenario-name>/<happ-name>.happ` from the project root.
 fn main() -> HappBuilderResult {
     println!("cargo:rerun-if-env-changed=SKIP_HAPP_BUILD");
     if std::env::var("SKIP_HAPP_BUILD").is_ok() {
