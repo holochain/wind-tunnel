768dc0e77d16562ccc91c1603b4fb3fce2388661
Jost <jost-s@users.noreply.github.com>
2025-03-14 11:35:11 -0600

    feat(kitsune_continuous_flow): configure number of messages with env var

--- bindings/kitsune_runner/src/context.rs
+++ bindings/kitsune_runner/src/context.rs
@@ -5,6 +5,7 @@ use wind_tunnel_runner::prelude::UserValuesConstraint;
 /// Kitsune specific agent context values.
 #[derive(Debug, Default)]
 pub struct KitsuneAgentContext {
+    /// The chatter instance.
     pub(crate) chatter: Option<Arc<WtChatter>>,
 }
 
@@ -12,14 +13,14 @@ impl UserValuesConstraint for KitsuneAgentContext {}
 
 impl KitsuneAgentContext {
     /// Get chatter instance.
-    pub fn get_chatter(&self) -> Arc<WtChatter> {
+    pub fn chatter(&self) -> Arc<WtChatter> {
         self.chatter.clone().expect(
             "chatter is not set, did you forget to call `create_chatter` in your agent setup?",
         )
     }
 
     /// Get chatter id.
-    pub fn get_chatter_id(&self) -> String {
+    pub fn chatter_id(&self) -> String {
         self.chatter
             .clone()
             .expect(
