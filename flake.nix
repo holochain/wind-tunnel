955120a46a1b5e6353497696018cee3473bd369a
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-05-21 20:37:55 +0100

    ci: Use less disk space (#185)
    * ci: Use less disk space
    
    * chore: Remove amber, no longer used
    
    * chore: Avoid re-building cached binaries
    
    * ci: prefer cargo for running scenarios rather than mixing and building the framework twice
    
    * ci: fix typo

--- flake.nix
+++ flake.nix
@@ -7,20 +7,10 @@
 
     holonix = {
       url = "github:holochain/holonix?ref=main-0.4";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
-        crane.follows = "crane";
-        rust-overlay.follows = "rust-overlay";
-      };
     };
 
     kitsune2 = {
       url = "github:holochain/kitsune2?ref=v0.0.1-alpha6";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        crane.follows = "crane";
-      };
     };
 
     crane = {
@@ -30,21 +20,6 @@
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # TODO should be followed correctly by amber for nixpkgs, contribute upstream.
-    naersk = {
-      url = "github:nix-community/naersk?ref=master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    amber = {
-      url = "github:thetasinner/amber?ref=master";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        naersk.follows = "naersk";
-        rust-overlay.follows = "rust-overlay";
-      };
     };
   };
 
@@ -101,7 +76,6 @@
                 pkgs.httpie
                 unfreePkgs.nomad
                 inputs'.holonix.packages.hn-introspect
-                inputs'.amber.packages.default
               ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
                 pkgs.darwin.apple_sdk.frameworks.Security
                 pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
@@ -149,7 +123,7 @@
   });
 
   nixConfig = {
-    substituters = [ "https://cache.nixos.org" "https://holochain-ci.cachix.org" "https://holochain-wind-tunnel.cachix.org" ];
-    trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8=" "holochain-wind-tunnel.cachix.org-1:tnSm+7Y3hDKOc9xLdoVMuInMA2AQ0R/99Ucz5edYGJw=" ];
+    substituters = [ "https://cache.nixos.org" "https://holochain-ci.cachix.org" ];
+    trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8=" ];
   };
 }
