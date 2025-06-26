3e2045734d890802389d922e8b55e9c078bec5cb
Callum Dunster <cdunster@users.noreply.github.com>
2025-06-26 12:42:41 +0200

    feat: path_validation add simple path validation to find root
    The path is considered valid if it is connected to the root hash.

--- zomes/path_validated/integrity/src/lib.rs
+++ zomes/path_validated/integrity/src/lib.rs
@@ -1,3 +1,4 @@
+use hdi::hash_path::path::root_hash;
 use hdi::prelude::*;
 
 #[hdk_entry_helper]
@@ -26,13 +27,43 @@ fn validate(op: Op) -> ExternResult<ValidateCallbackResult> {
         | FlatOp::RegisterAgentActivity(
             OpActivity::InitZomesComplete { .. } | OpActivity::CreateEntry { .. },
         ) => Ok(ValidateCallbackResult::Valid),
-        FlatOp::StoreRecord(OpRecord::CreateLink { .. }) => Ok(ValidateCallbackResult::Valid),
         FlatOp::RegisterAgentActivity(OpActivity::CreateLink { .. }) => {
             Ok(ValidateCallbackResult::Valid)
         }
         FlatOp::RegisterCreateLink { link_type, .. } => match link_type {
             LinkTypes::Path => Ok(ValidateCallbackResult::Valid),
         },
+        FlatOp::StoreRecord(OpRecord::CreateLink {
+            base_address,
+            action,
+            ..
+        }) => {
+            let mut base_address_lookup = base_address.clone();
+            loop {
+                // Find the root
+                if base_address_lookup == root_hash()? {
+                    break;
+                }
+                if base_address_lookup.into_entry_hash().is_none() {
+                    return Ok(ValidateCallbackResult::Invalid(
+                        "Base address is not valid entry hash".to_string(),
+                    ));
+                }
+                if let Action::CreateLink(prev_create_link) =
+                    must_get_action(action.prev_action.clone())?
+                        .hashed
+                        .as_content()
+                {
+                    base_address_lookup = prev_create_link.base_address.clone();
+                } else {
+                    return Ok(ValidateCallbackResult::Invalid(
+                        "Cannot get prev_action".to_string(),
+                    ));
+                }
+            }
+
+            Ok(ValidateCallbackResult::Valid)
+        }
         _ => Ok(ValidateCallbackResult::Invalid(format!(
             "Validation not supported for type: {:?}",
             op
