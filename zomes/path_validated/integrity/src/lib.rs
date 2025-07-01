c1fc563909c1ef01319a56db5b7161675cf38903
Callum Dunster <cdunster@users.noreply.github.com>
2025-07-01 17:13:22 +0200

    feat: path_validation remove validation

--- zomes/path_validated/integrity/src/lib.rs
+++ zomes/path_validated/integrity/src/lib.rs
@@ -1,4 +1,3 @@
-use hdi::hash_path::path::root_hash;
 use hdi::prelude::*;
 
 #[hdk_entry_helper]
@@ -17,38 +16,4 @@ pub enum EntryTypes {
 #[hdk_link_types]
 pub enum LinkTypes {
     Path,
-}
-
-#[hdk_extern]
-fn validate(op: Op) -> ExternResult<ValidateCallbackResult> {
-    match op.flattened::<EntryTypes, LinkTypes>()? {
-        FlatOp::StoreRecord(OpRecord::CreateLink {
-            link_type: LinkTypes::Path,
-            base_address,
-            action,
-            ..
-        }) => {
-            let mut base_address_lookup = base_address.clone();
-            loop {
-                // Find the root
-                if base_address_lookup == root_hash()? {
-                    break;
-                }
-                if let Action::CreateLink(prev_create_link) =
-                    must_get_action(action.prev_action.clone())?
-                        .hashed
-                        .as_content()
-                {
-                    base_address_lookup = prev_create_link.base_address.clone();
-                } else {
-                    return Ok(ValidateCallbackResult::Invalid(
-                        "Cannot get prev_action".to_string(),
-                    ));
-                }
-            }
-
-            Ok(ValidateCallbackResult::Valid)
-        }
-        _ => Ok(ValidateCallbackResult::Valid),
-    }
 }
