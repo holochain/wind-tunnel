4cc4b70407049e2fe8c270ac5dc72537dabd9e8b
Callum Dunster <cdunster@users.noreply.github.com>
2025-08-29 17:22:59 +0200

    feat(runner): add hard-coded sandbox clean, create, and run logic

--- bindings/runner/src/runner_context.rs
+++ bindings/runner/src/runner_context.rs
@@ -1,9 +1,12 @@
 use wind_tunnel_runner::prelude::UserValuesConstraint;
 
+use crate::prelude::HolochainSandbox;
+
 /// Holochain-specific context values for the [wind_tunnel_runner::prelude::RunnerContext].
 #[derive(Default, Debug)]
 pub struct HolochainRunnerContext {
     pub(crate) app_ws_url: Option<String>,
+    pub(crate) holochain_sandbox: Option<HolochainSandbox>,
 }
 
 impl UserValuesConstraint for HolochainRunnerContext {}
