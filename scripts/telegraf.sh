1958bfa9b5095feb9b926008a8e00e4b82a4262e
damien <34140573+ddd-mtl@users.noreply.github.com>
2025-09-08 14:56:36 -0400

    feat: Support for importing Holochain metrics into InfluxDB (#254)

--- scripts/telegraf.sh
+++ scripts/telegraf.sh
@@ -3,15 +3,6 @@
 WT_METRICS_DIR="$(pwd)/telegraf/metrics"
 export WT_METRICS_DIR
 
-start_telegraf() {
-    if [[ -z "$INFLUX_TOKEN" ]]; then
-        echo "INFLUX_TOKEN is not set, please run \`use_influx\` first"
-        return 1
-    fi
-
-    telegraf --config "$(pwd)/telegraf/telegraf.local.conf"
-}
-
 start_host_metrics_telegraf() {
     telegraf --config "$(pwd)/telegraf/telegraf.host.conf"
 }
