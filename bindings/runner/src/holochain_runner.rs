49c5ae04d67d41a0886b8fd4aefd8851df0f438f
Callum Dunster <cdunster@users.noreply.github.com>
2025-09-09 16:01:39 +0200

    docs(runner): add doc-comments to the holochain_runner module

--- bindings/runner/src/holochain_runner.rs
+++ bindings/runner/src/holochain_runner.rs
@@ -1,3 +1,5 @@
+//! Provides the ability to configure and run a Holochain conductor as a [`Child`] process.
+
 use std::{fs, path::PathBuf, process::Stdio, time::Duration};
 
 use anyhow::{anyhow, Context};
@@ -15,35 +17,58 @@ use tokio::{
 };
 use wind_tunnel_runner::prelude::WindTunnelResult;
 
+/// Used to build a [`HolochainConfig`], which is then passed into [`HolochainRunner::run`] to
+/// spawn a [`Child`] process running a Holochain conductor with the specified config.
 #[derive(Debug, Default)]
 pub struct HolochainConfigBuilder {
+    /// The path to the `holochain` binary used to start a conductor.
+    ///
+    /// If [`None`] when [`Self::build`] is called then it uses the binary in the user's `PATH`.
     bin_path: Option<PathBuf>,
+
+    /// If set when [`Self::build`] is called then an admin interface is created on the conductor
+    /// that is accessible via this port.
     admin_port: Option<u16>,
+
+    /// The root path where the generated config and the data for the Holochain conductor is
+    /// stored.
     conductor_config_root_path: Option<ConfigRootPath>,
+
+    /// The target arc factor for the conductor, leave as [`None`] to use the Holochain default or
+    /// set to `0` to be configured as a zero-arc conductor.
     target_arc_factor: Option<u32>,
 }
 
 impl HolochainConfigBuilder {
+    /// Set the target arc factor for the conductor, set to `0` to configure the conductor as a
+    /// zero-arc conductor.
     pub fn with_target_arc_factor(&mut self, target_arc_factor: u32) -> &mut Self {
         self.target_arc_factor = Some(target_arc_factor);
         self
     }
 
+    /// Override the path to the `holochain` binary that is used to start a conductor.
     pub(crate) fn with_bin_path(&mut self, path: impl Into<PathBuf>) -> &mut Self {
         self.bin_path = Some(path.into());
         self
     }
 
+    /// Create an admin interface on the conductor that is accessible via this port.
     pub(crate) fn with_admin_port(&mut self, port: u16) -> &mut Self {
         self.admin_port = Some(port);
         self
     }
 
+    /// Set the root path of the conductor where the generated config is written and the conductor
+    /// stores its data.
     pub(crate) fn with_conductor_root_path(&mut self, path: impl Into<PathBuf>) -> &mut Self {
         self.conductor_config_root_path = Some(path.into().into());
         self
     }
 
+    /// Build a [`HolochainConfig`], applying the overrides and defaults where appropriate.
+    ///
+    /// Returns an error if required fields are not set.
     pub(crate) fn build(self) -> WindTunnelResult<HolochainConfig> {
         let bin_path = self.bin_path.unwrap_or(PathBuf::from("holochain"));
         let conductor_config_root_path = self.conductor_config_root_path.ok_or(anyhow!(
@@ -79,22 +104,40 @@ impl HolochainConfigBuilder {
     }
 }
 
+/// The configuration of the conductor itself as managed by Wind Tunnel as well as the
+/// [`ConductorConfig`] that is written to the [`ConfigRootPath`] and passed to the conductor.
+///
+/// Must be created with a [`HolochainConfigBuilder`] by calling [`HolochainConfigBuilder::build`]
+/// once all fields are correctly set.
 #[derive(Debug)]
 pub struct HolochainConfig {
+    /// The path to the `holochain` binary used to start a conductor.
     bin_path: PathBuf,
+
+    /// The path where the generated config and the data for the Holochain conductor is stored.
     conductor_config_root_path: ConfigRootPath,
+
+    /// The conductor configuration that is written to a file and passed, as a path, to the
+    /// conductor when started.
     conductor_config: ConductorConfig,
 }
 
+/// Holds the [`Child`] process that is running the Holochain conductor, as well as the path to the
+/// directory that the conductor stores its data so that they can be cleaned up when
+/// [`Drop::drop`]'ed.
 #[derive(Debug)]
 pub struct HolochainRunner {
+    /// The [`Child`] process that is running the Holochain conductor.
     _holochain_handle: Child,
+
+    /// The path where the running conductor stores its data.
     conductor_root_path: PathBuf,
 }
 
 impl HolochainRunner {
     /// Runs an instance of Holochain, using the passed [`HolochainConfig`]. Storing the [`Child`]
-    /// process internally so it can be gracefully shutdown on [`Drop::drop`].
+    /// process internally so it can be gracefully shutdown and clean up directories on
+    /// [`Drop::drop`].
     pub async fn run(config: &HolochainConfig) -> WindTunnelResult<Self> {
         let conductor_root_path = config.conductor_config_root_path.to_path_buf();
         if !fs::exists(&conductor_root_path)? {
