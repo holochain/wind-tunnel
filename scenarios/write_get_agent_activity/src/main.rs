fccfffd7f1c109b8439112c76caec48405119a5f
Matt Gabrenya <matt@buildyourweb.app>
2025-09-17 19:17:05 -0700

    refactor: only use 1 agent, who gets their own activity - we don't care about testing dht sync so getting a different agent's activity does not seem relevant

--- scenarios/write_get_agent_activity/src/main.rs
+++ scenarios/write_get_agent_activity/src/main.rs
@@ -1,17 +1,8 @@
+use holochain_types::prelude::ActionHash;
 use holochain_types::prelude::AgentActivity;
-use holochain_types::prelude::{ActionHash, AgentPubKey};
 use holochain_wind_tunnel_runner::prelude::*;
 use holochain_wind_tunnel_runner::scenario_happ_path;
-use kitsune2_api::AgentInfoSigned;
-use kitsune2_core::Ed25519Verifier;
-use std::time::{Duration, Instant};
-
-#[derive(Debug, Default)]
-pub struct ScenarioValues {
-    write_peer: Option<AgentPubKey>,
-}
-
-impl UserValuesConstraint for ScenarioValues {}
+use std::time::Instant;
 
 fn setup(ctx: &mut RunnerContext<HolochainRunnerContext>) -> HookResult {
     configure_app_ws_url(ctx)?;
@@ -19,7 +10,7 @@ fn setup(ctx: &mut RunnerContext<HolochainRunnerContext>) -> HookResult {
 }
 
 fn agent_setup(
-    ctx: &mut AgentContext<HolochainRunnerContext, HolochainAgentContext<ScenarioValues>>,
+    ctx: &mut AgentContext<HolochainRunnerContext, HolochainAgentContext>,
 ) -> HookResult {
     install_app(
         ctx,
@@ -27,93 +18,57 @@ fn agent_setup(
         &"agent_activity".to_string(),
     )?;
 
-    try_wait_for_min_agents(ctx, Duration::from_secs(120))?;
-
     Ok(())
 }
 
-fn agent_behaviour_write(
-    ctx: &mut AgentContext<HolochainRunnerContext, HolochainAgentContext<ScenarioValues>>,
+fn agent_behaviour(
+    ctx: &mut AgentContext<HolochainRunnerContext, HolochainAgentContext>,
 ) -> HookResult {
+    let reporter = ctx.runner_context().reporter();
+
+    // Create an entry
     let _: ActionHash = call_zome(
         ctx,
         "agent_activity",
         "create_sample_entry",
         "this is a test entry value",
     )?;
-    Ok(())
-}
 
-fn agent_behaviour_get_agent_activity(
-    ctx: &mut AgentContext<HolochainRunnerContext, HolochainAgentContext<ScenarioValues>>,
-) -> HookResult {
-    let reporter = ctx.runner_context().reporter();
-    let saved_write_peer = ctx.get_mut().scenario_values.write_peer.clone();
+    // Get my own agent activity
+    let now = Instant::now();
+    let activity: AgentActivity = call_zome(
+        ctx,
+        "agent_activity",
+        "get_agent_activity_full",
+        ctx.get().cell_id().agent_pubkey(),
+    )?;
+    let elapsed = now.elapsed();
 
-    match saved_write_peer {
-        Some(write_peer) => {
-            let now = Instant::now();
-            let activity: AgentActivity = call_zome(
-                ctx,
-                "agent_activity",
-                "get_agent_activity_full",
-                write_peer.clone(),
-            )?;
-            let elapsed = now.elapsed();
-
-            reporter.add_custom(
-                ReportMetric::new("write_get_agent_activity")
-                    .with_tag("agent", write_peer.to_string())
-                    .with_field(
-                        "highest_observed_action_seq",
-                        activity.highest_observed.map_or(0, |v| v.action_seq),
-                    )
-                    .with_field("value", elapsed.as_secs_f64()),
-            );
-        }
-        _ => {
-            let client = ctx.get().app_client();
-            let cell_id = ctx.get().cell_id();
-            let new_peers: Vec<AgentPubKey> =
-                ctx.runner_context()
-                    .executor()
-                    .execute_in_place(async move {
-                        let agent_infos_encoded: Vec<String> = client.agent_info(None).await?;
-
-                        let mut agent_infos = Vec::new();
-                        for info in agent_infos_encoded {
-                            let a = AgentInfoSigned::decode(&Ed25519Verifier, info.as_bytes())?;
-                            agent_infos.push(AgentPubKey::from_k2_agent(&a.agent))
-                        }
-                        let peer_list = agent_infos
-                            .into_iter()
-                            .filter(|k| k != cell_id.agent_pubkey()) // Don't include ourselves!
-                            .collect::<Vec<_>>();
-
-                        Ok(peer_list)
-                    })?;
-
-            ctx.get_mut().scenario_values.write_peer = new_peers.first().cloned();
-        }
-    }
+    reporter.add_custom(
+        ReportMetric::new("write_get_agent_activity")
+            .with_field(
+                "highest_observed_action_seq",
+                activity.highest_observed.map_or(0, |v| v.action_seq),
+            )
+            .with_field("value", elapsed.as_secs_f64()),
+    );
 
     Ok(())
 }
 
 fn main() -> WindTunnelResult<()> {
-    let builder = ScenarioDefinitionBuilder::<
-        HolochainRunnerContext,
-        HolochainAgentContext<ScenarioValues>,
-    >::new_with_init(env!("CARGO_PKG_NAME"))
-    .with_default_duration_s(60)
-    .use_setup(setup)
-    .use_agent_setup(agent_setup)
-    .use_named_agent_behaviour("write", agent_behaviour_write)
-    .use_named_agent_behaviour("get_agent_activity", agent_behaviour_get_agent_activity)
-    .use_agent_teardown(|ctx| {
-        uninstall_app(ctx, None).ok();
-        Ok(())
-    });
+    let builder =
+        ScenarioDefinitionBuilder::<HolochainRunnerContext, HolochainAgentContext>::new_with_init(
+            env!("CARGO_PKG_NAME"),
+        )
+        .with_default_duration_s(60)
+        .use_setup(setup)
+        .use_agent_setup(agent_setup)
+        .use_agent_behaviour(agent_behaviour)
+        .use_agent_teardown(|ctx| {
+            uninstall_app(ctx, None).ok();
+            Ok(())
+        });
 
     run(builder)?;
 
