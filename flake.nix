5d4087aad91d311cc6d205ff495db4f063161b9b
veeso <christian.visintin@veeso.dev>
2025-11-07 14:39:15 +0100

    chore: allow unfree

--- flake.nix
+++ flake.nix
@@ -451,6 +451,7 @@
   };
 
   nixConfig = {
+    allowUnfree = true;
     substituters = [ "https://cache.nixos.org" "https://holochain-ci.cachix.org" ];
     trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8=" ];
   };
