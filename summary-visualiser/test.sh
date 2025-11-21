8384a9dbce044464a6a64f735e96025905ec6880
Paul d'Aoust <paul.daoust@holo.host>
2025-11-21 14:54:32 -0800

    feat: scenario template for `remote_call_rate` (#349)
    * fix: styling tweaks to scenario summary
    
    * feat: support more scenario summary fields
    
    * feat: remote_call_rate scenario template
    
    * fix: top-align metric labels for remote_call_rate
    
    * fix: default value for empty behaviour name
    
    * chore: real test data for remote_call_rate
    
    * fix: broken path to test data JSON
    
    * chore: refactor smoke test for summary HTML generator
    
    * fix: small tweaks to k/v data in scenario summary
    
    * fix: smoke test looking for wrong CSS class
    
    * chore: refactor scenario loop to include wrapper section that formats class name predictably
    
    * chore: fix old test label
    
    * fix: make HTML generator smoke tag messages consistent
    
    * feat: composite key helper
    
    * fix: edit remote_call_rate description

--- summary-visualiser/test.sh
+++ summary-visualiser/test.sh
@@ -5,16 +5,33 @@
 
 script_dir=$(dirname "$0")
 
+# Load up some JSON data and try to generate an HTML page from it.
+# Arguments:
+#   $1: a JSON string
+#   $2: a description of the JSON string being tested
+#   $3: the scenario name
 test_output() {
     html_output=$(echo "$1" | "$script_dir/generate.sh")
-    expected_element_in_output=$(echo "$html_output" | grep '<section class="scenario scenario-dht-sync-lag">')
+    # Test for an expected HTML element that shows it found a template for this scenario.
+    expected_html_tag="<section class=\"scenario scenario-${3//_/-}\">"
+    expected_element_in_output=$(echo "$html_output" | grep "$expected_html_tag")
     if [ -n "$expected_element_in_output" ]; then
-        echo "Found expected .scenario-dht-sync-lag element in output for $2"
+        echo "Found expected .scenario-${3//_/-} element in output for $2"
     else
-        echo "Couldn't find expected .scenario-dht-sync-lag element in output for $2"
+        echo "Couldn't find expected .scenario-${3//_/-} element in output for $2"
         exit 1
     fi
 }
 
-test_output "[$(cat "$script_dir/../summariser/test_data/3_summary_outputs/dht_sync_lag-3a1e33ccf661bd873966c539d4d227e703e1496fb54bb999f7be30a3dd493e51.json")]" "well-populated sample JSON"
-test_output "$(cat "$script_dir/test_data/dht_sync_lag.json")" "real JSON snapshot with some null metrics"
+# Load up both a representative sample JSON file and a real sample taken from a CI run,
+# and try to turn both of them into HTML files.
+# Arguments:
+#   $1: the scenario name, will be used in the filename and the test messaging
+#   $2: the unique identifier for the scenario name's JSON file in summariser/test_data/3_summary_outputs/
+test_output_sample_real() {
+    test_output "[$(cat "$script_dir/../summariser/test_data/3_summary_outputs/$1-$2.json")]" "well-populated sample JSON - $1" "$1"
+    test_output "$(cat "$script_dir/test_data/$1.json")" "real JSON snapshot - $1" "$1"
+}
+
+test_output_sample_real "dht_sync_lag" "3a1e33ccf661bd873966c539d4d227e703e1496fb54bb999f7be30a3dd493e51"
+test_output_sample_real "remote_call_rate" "f92e98962b23bfe104373a735dd9af8eb363e347a0c528902d4a2aaa8351cd74"
