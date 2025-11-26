55cbad9af256af62d60b54e52ee995deb797936e
Paul d'Aoust <paul.daoust@holo.host>
2025-11-26 14:26:26 -0800

    chore: refactor template names, snake_case rather than camelCase

--- summary-visualiser/generate.sh
+++ summary-visualiser/generate.sh
@@ -21,7 +21,7 @@ done
 #   (note: this requires `SCENARIO_TEMPLATES_DIR` to be set)
 SCENARIO_TEMPLATES_DIR="$script_dir/templates/scenarios" gomplate \
     -c .="${1:-stdin:///in.json}" \
-    -t js="$script_dir/assets/windTunnel.js" \
+    -t js="$script_dir/assets/wind_tunnel.js" \
     -t css="$script_dir/assets/wind_tunnel.css" \
     "${template_args[@]}" \
     --plugin scenario_template_exists="$script_dir/scenario_template_exists.sh" \
