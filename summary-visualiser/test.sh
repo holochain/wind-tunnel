78562d1b4b3a022c99fcd22e9628cf12b9657be1
Paul d'Aoust <paul.daoust@holo.host>
2025-11-28 13:23:05 -0800

    chore: add zero_arc_create_and_read to smoke test

--- summary-visualiser/test.sh
+++ summary-visualiser/test.sh
@@ -31,4 +31,5 @@ smoke_test_scenario() {
 smoke_test_scenario "dht_sync_lag"
 smoke_test_scenario "remote_call_rate"
 smoke_test_scenario "validation_receipts"
+smoke_test_scenario "zero_arc_create_and_read"
 smoke_test_scenario "zero_arc_create_data"
