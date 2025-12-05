49b18cf2e3cf80300e8e9e511037865391a9266d
Paul d'Aoust <paul.daoust@holo.host>
2025-12-05 10:04:46 -0800

    chore: add local_signals to smoke test

--- summary-visualiser/test.sh
+++ summary-visualiser/test.sh
@@ -29,6 +29,7 @@ smoke_test_scenario() {
 }
 
 smoke_test_scenario "dht_sync_lag"
+smoke_test_scenario "local_signals"
 smoke_test_scenario "remote_call_rate"
 smoke_test_scenario "validation_receipts"
 smoke_test_scenario "zero_arc_create_and_read"
