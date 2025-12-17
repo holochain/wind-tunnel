e77c64415a455ea36aa10faabfd32c8b67e3d6a4
mattyg <matt@buildyourweb.app>
2025-12-17 03:27:31 -0700

    refactor: move code to get holochain build info from framework runner to holochain bindings (#396)
    * refactor: move code to get holochain build info from framework runner -> holochain bindings runner, since it is holochain scenario specific
    
    * test: update summarizer fixture outputs

--- scenarios/zero_arc_create_data_validated/src/main.rs
+++ scenarios/zero_arc_create_data_validated/src/main.rs
@@ -147,6 +147,7 @@ fn main() -> WindTunnelResult<()> {
         HolochainAgentContext<ScenarioValues>,
     >::new_with_init(env!("CARGO_PKG_NAME"))
     .with_default_duration_s(60)
+    .use_build_info(conductor_build_info)
     .use_agent_setup(agent_setup)
     .use_named_agent_behaviour("zero", agent_behaviour_zero)
     .use_named_agent_behaviour("full", agent_behaviour_full)
