88271b2ff402df465bbe45951488bbbf5c0061a8
Paul d'Aoust <paul.daoust@holo.host>
2025-12-19 10:56:08 -0800

    fix: remove unneeded deps for check-all

--- flake.nix
+++ flake.nix
@@ -363,8 +363,6 @@
               pkgs.shellcheck
               pkgs.taplo
               pkgs.yamlfmt
-              pkgs.gomplate
-              pkgs.html-tidy
               pkgs.statix
             ];
             text = ''
