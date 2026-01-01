05008bc5dbbcba6fb8700047c094eb5bc6063945
Matt Gabrenya <matt@buildyourweb.app>
2025-12-31 19:26:31 -0700

    build: bump nixpkgs to 25.11 -- crane does not support 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for Holochain testing";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
