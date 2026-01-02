ab0d65f8fb75cf99f5f42c044fa102cb053338fa
Paul d'Aoust <paul.daoust@holo.host>
2026-01-02 12:56:27 -0800

    fix: gnugrep, not grep

--- flake.nix
+++ flake.nix
@@ -496,8 +496,8 @@
               name = "summary-visualiser-smoke-test";
               runtimeInputs = [
                 pkgs.coreutils
+                pkgs.gnugrep
                 pkgs.gomplate
-                pkgs.grep
                 pkgs.html-tidy
               ];
               text = ''
