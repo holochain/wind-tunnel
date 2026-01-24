498f6d3edd4356455c6cbd879c7ec1716c3309f0
Matt <matt@buildyourweb.app>
2026-01-23 17:57:14 -0700

    feat: include holochain_p2p metrics in relevent scenario summarizers

--- summariser/src/scenario/mixed_arc_get_agent_activity.rs
+++ summariser/src/scenario/mixed_arc_get_agent_activity.rs
@@ -4,6 +4,7 @@ use crate::frame::LoadError;
 use crate::model::{
     PartitionedGaugeStats, PartitionedRateStats, PartitionedTimingStats, SummaryOutput,
 };
+use crate::query::holochain_p2p_metrics::{HolochainP2pMetrics, query_holochain_p2p_metrics};
 use crate::{analyze, query};
 use analyze::partitioned_timing_stats;
 use anyhow::Context;
@@ -21,6 +22,7 @@ struct MixedArcGetAgentActivitySummary {
     retrieval_errors: PartitionedTimingStats,
     open_connections: PartitionedGaugeStats,
     error_count: usize,
+    holochain_p2p_metrics: HolochainP2pMetrics,
 }
 
 pub(crate) async fn summarize_mixed_arc_get_agent_activity(
@@ -141,7 +143,8 @@ pub(crate) async fn summarize_mixed_arc_get_agent_activity(
             retrieval_errors: retrieval_errors_count,
             open_connections: partitioned_gauge_stats(open_connections, "value", &["behaviour"])
                 .context("Open connections")?,
-            error_count: query::zome_call_error_count(client, &summary).await?,
+            error_count: query::zome_call_error_count(client.clone(), &summary).await?,
+            holochain_p2p_metrics: query_holochain_p2p_metrics(&client, &summary).await?,
         },
         host_metrics,
     )
