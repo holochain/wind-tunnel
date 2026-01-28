354d83c6b9b83beeb00b0cc395ba223ae7074ee1
Matt <matt@buildyourweb.app>
2026-01-28 12:03:47 -0700

    feat: add metric 'hc.holochain_p2p.handle_request.ignored.requests' to relevant scenarios

--- summariser/src/scenario/remote_call_rate.rs
+++ summariser/src/scenario/remote_call_rate.rs
@@ -1,7 +1,8 @@
 use crate::aggregator::HostMetricsAggregator;
 use crate::model::{PartitionedTimingStats, StandardTimingsStats, SummaryOutput};
 use crate::query::holochain_metrics::{
-    query_p2p_handle_request_duration, query_p2p_request_duration,
+    query_p2p_handle_request_duration, query_p2p_handle_request_ignored_count,
+    query_p2p_request_duration,
 };
 use crate::{analyze, query};
 use analyze::partitioned_timing_stats;
@@ -16,6 +17,7 @@ struct RemoteCallRateSummary {
     error_count: usize,
     p2p_request_duration: Option<StandardTimingsStats>,
     p2p_handle_request_duration: Option<StandardTimingsStats>,
+    p2p_handle_request_ignored_count: u64,
 }
 
 pub(crate) async fn summarize_remote_call_rate(
@@ -62,6 +64,10 @@ pub(crate) async fn summarize_remote_call_rate(
             p2p_request_duration: query_p2p_request_duration(&client, &summary).await?,
             p2p_handle_request_duration: query_p2p_handle_request_duration(&client, &summary)
                 .await?,
+            p2p_handle_request_ignored_count: query_p2p_handle_request_ignored_count(
+                &client, &summary,
+            )
+            .await?,
         },
         host_metrics,
     )
