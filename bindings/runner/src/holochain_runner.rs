77fda387cda1156e5b36ba52dfa3d144158526bd
Matt <matt@buildyourweb.app>
2026-02-11 17:14:59 -0700

    feat: shutdown and restart conductor functions for holochain binding

--- bindings/runner/src/holochain_runner.rs
+++ bindings/runner/src/holochain_runner.rs
@@ -1,16 +1,14 @@
 //! Provides the ability to configure and run a Holochain conductor as a [`Child`] process.
 
-use std::{
-    fs,
-    path::{Path, PathBuf},
-    process::Stdio,
-    time::Duration,
-};
+use std::{fs, path::PathBuf, process::Stdio, time::Duration};
 
 use anyhow::{Context, anyhow};
 use holochain_conductor_api::{
     AdminInterfaceConfig, InterfaceDriver,
-    conductor::{ConductorConfig, KeystoreConfig, paths::ConfigRootPath},
+    conductor::{
+        ConductorConfig, KeystoreConfig,
+        paths::{ConfigFilePath, ConfigRootPath},
+    },
 };
 use holochain_conductor_config::config::write_config;
 use holochain_types::websocket::AllowedOrigins;
@@ -42,11 +40,15 @@ pub struct HolochainConfigBuilder {
 
     /// The root path where the generated config and the data for the Holochain conductor is
     /// stored.
-    conductor_config_root_path: Option<ConfigRootPath>,
+    conductor_root_path: Option<PathBuf>,
 
     /// The target arc factor for the conductor, leave as [`None`] to use the Holochain default or
     /// set to `0` to be configured as a zero-arc conductor.
     target_arc_factor: Option<u32>,
+
+    /// The path where influxive metrics will be written, by setting the
+    /// env variable HOLOCHAIN_INFLUXIVE_FILE for the holochain process.
+    metrics_path: Option<PathBuf>,
 }
 
 impl HolochainConfigBuilder {
@@ -78,7 +80,12 @@ impl HolochainConfigBuilder {
     /// Set the root path of the conductor where the generated config is written and the conductor
     /// stores its data.
     pub(crate) fn with_conductor_root_path(&mut self, path: impl Into<PathBuf>) -> &mut Self {
-        self.conductor_config_root_path = Some(path.into().into());
+        self.conductor_root_path = Some(path.into());
+        self
+    }
+
+    pub(crate) fn with_metrics_path(&mut self, path: impl Into<PathBuf>) -> &mut Self {
+        self.metrics_path = Some(path.into());
         self
     }
 
@@ -87,13 +94,13 @@ impl HolochainConfigBuilder {
     /// Returns an error if required fields are not set.
     pub(crate) fn build(self) -> WindTunnelResult<HolochainConfig> {
         let bin_path = self.bin_path.unwrap_or(PathBuf::from("holochain"));
-        let conductor_config_root_path = self.conductor_config_root_path.ok_or(anyhow!(
-            "Conductor config root path not set, this should be set by the Wind Tunnel runner"
+        let conductor_root_path = self.conductor_root_path.ok_or(anyhow!(
+            "Conductor root path not set, this should be set by the Wind Tunnel runner"
         ))?;
-        let conductor_data_root_path = conductor_config_root_path.is_also_data_root_path();
+        let keystore_path = conductor_root_path.clone().join("ks");
         let mut conductor_config = if let Some(admin_port) = self.admin_port {
             ConductorConfig {
-                data_root_path: Some(conductor_data_root_path.clone()),
+                data_root_path: Some(conductor_root_path.clone().into()),
                 admin_interfaces: Some(vec![AdminInterfaceConfig {
                     driver: InterfaceDriver::Websocket {
                         port: admin_port,
@@ -102,7 +109,7 @@ impl HolochainConfigBuilder {
                     },
                 }]),
                 keystore: KeystoreConfig::LairServerInProc {
-                    lair_root: Some(conductor_data_root_path.try_into()?),
+                    lair_root: Some(keystore_path.clone().into()),
                 },
                 ..Default::default()
             }
@@ -112,12 +119,16 @@ impl HolochainConfigBuilder {
         if let Some(target_arc_factor) = self.target_arc_factor {
             conductor_config.network.target_arc_factor = target_arc_factor;
         }
+        let metrics_path = self.metrics_path.ok_or(anyhow!(
+            "Metrics path not set, this should be set by the Wind Tunnel runner"
+        ))?;
 
         Ok(HolochainConfig {
             bin_path,
             agent_name: self.agent_name,
-            conductor_config_root_path,
+            conductor_root_path,
             conductor_config,
+            metrics_path,
         })
     }
 }
@@ -127,7 +138,7 @@ impl HolochainConfigBuilder {
 ///
 /// Must be created with a [`HolochainConfigBuilder`] by calling [`HolochainConfigBuilder::build`]
 /// once all fields are correctly set.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct HolochainConfig {
     /// The path to the `holochain` binary used to start a conductor.
     bin_path: PathBuf,
@@ -136,11 +147,15 @@ pub struct HolochainConfig {
     agent_name: Option<String>,
 
     /// The path where the generated config and the data for the Holochain conductor is stored.
-    conductor_config_root_path: ConfigRootPath,
+    conductor_root_path: PathBuf,
 
     /// The conductor configuration that is written to a file and passed, as a path, to the
     /// conductor when started.
     conductor_config: ConductorConfig,
+
+    /// The path where influxive metrics will be written, by setting the
+    /// env variable HOLOCHAIN_INFLUXIVE_FILE for the holochain process.
+    metrics_path: PathBuf,
 }
 
 /// Holds the [`Child`] process that is running the Holochain conductor, as well as the path to the
@@ -148,46 +163,52 @@ pub struct HolochainConfig {
 /// [`Drop::drop`]'ed.
 #[derive(Debug)]
 pub struct HolochainRunner {
-    /// The [`Child`] process that is running the Holochain conductor.
-    _holochain_handle: Child,
+    /// The config of this holochain runner instance.
+    config: HolochainConfig,
 
-    /// The path where the running conductor stores its data.
-    conductor_root_path: PathBuf,
+    /// The [`Child`] process that is running the Holochain conductor.
+    holochain_handle: Option<Child>,
 }
 
 impl HolochainRunner {
-    /// Runs an instance of Holochain, using the passed [`HolochainConfig`]. Storing the [`Child`]
-    /// process internally so it can be gracefully shutdown and clean up directories on
-    /// [`Drop::drop`].
-    pub async fn run(
-        config: &HolochainConfig,
-        holochain_metrics_path: &Path,
-    ) -> WindTunnelResult<Self> {
-        let conductor_root_path: PathBuf = config.conductor_config_root_path.to_path_buf();
-        fs::create_dir_all(&conductor_root_path).with_context(|| {
+    /// Create a holochain runner with the provided config, but do not start it.
+    pub fn create(config: &HolochainConfig) -> WindTunnelResult<Self> {
+        fs::create_dir_all(&config.conductor_root_path).with_context(|| {
             format!(
-                "Failed to create conductor root directory '{}'",
-                conductor_root_path.display()
+                "Failed to create cChonductor root directory '{}'",
+                config.conductor_root_path.display()
             )
         })?;
-        let conductor_root_path = conductor_root_path.canonicalize()?;
+        let conductor_root_path = config.conductor_root_path.canonicalize()?;
 
         log::trace!(
             "Writing conductor config file to '{}'",
             conductor_root_path.display()
         );
-        let conductor_config_path = write_config(
-            config.conductor_config_root_path.clone(),
+        write_config(
+            config.conductor_root_path.clone().into(),
             &config.conductor_config,
         )
-        .context("Failed to write conductor config file")?
-        .to_path_buf();
+        .context("Failed to write conductor config file")?;
+
+        Ok(Self {
+            config: config.clone(),
+            holochain_handle: None,
+        })
+    }
+
+    /// Runs an instance of Holochain from the stored [`HolochainConfig`]. Storing the [`Child`]
+    /// process internally so it can be gracefully shutdown and clean up directories on
+    /// [`Drop::drop`].
+    pub async fn run(&mut self) -> WindTunnelResult<()> {
+        let config_root_path: ConfigRootPath = self.config.conductor_root_path.clone().into();
+        let config_file_path: ConfigFilePath = config_root_path.into();
 
         log::info!("Running a Holochain conductor");
-        let mut holochain_handle = Command::new(&config.bin_path)
-            .env("HOLOCHAIN_INFLUXIVE_FILE", holochain_metrics_path)
+        let mut holochain_handle = Command::new(&self.config.bin_path)
+            .env("HOLOCHAIN_INFLUXIVE_FILE", self.config.metrics_path.clone())
             .arg("--config-path")
-            .arg(conductor_config_path)
+            .arg(config_file_path.as_os_str())
             .arg("--piped")
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
@@ -210,6 +231,7 @@ impl HolochainRunner {
             .take()
             .context("Failed to get stdout for the running Holochain conductor")?;
 
+        let agent_name = self.config.agent_name.clone();
         timeout(Duration::from_secs(30), async move {
             let mut stdout_lines = BufReader::new(holochain_stdout).lines();
             loop {
@@ -219,7 +241,7 @@ impl HolochainRunner {
                     .context("Failed to read line from Holochain conductor stdout")?
                     .ok_or(anyhow!("Holochain conductor shutdown before it was ready"))?;
                 let mut log_target = "holochain_conductor".to_string();
-                if let Some(agent_name) = &config.agent_name {
+                if let Some(agent_name) = &agent_name {
                     log_target.push_str("::");
                     log_target.push_str(agent_name);
                 }
@@ -240,26 +262,69 @@ impl HolochainRunner {
         .await
         .context("Timed-out whilst waiting for the Holochain conductor to be ready")??;
 
-        Ok(Self {
-            _holochain_handle: holochain_handle,
-            conductor_root_path,
-        })
+        self.holochain_handle = Some(holochain_handle);
+
+        Ok(())
+    }
+
+    pub fn shutdown(&mut self) {
+        log::info!("Shutting down holochain conductor");
+
+        // This will drop the child process, which will shutdown the conductor
+        self.holochain_handle = None;
     }
 }
 
 impl Drop for HolochainRunner {
     fn drop(&mut self) {
         log::trace!("Cleaning up the conductor files");
-        if let Err(err) = fs::remove_dir_all(&self.conductor_root_path) {
+        if let Err(err) = fs::remove_dir_all(&self.config.conductor_root_path) {
             log::error!("Failed to cleanup the conductor files: {err}");
         } else {
             log::info!("Successfully cleaned up the conductor files");
         }
 
-        if let Some(parent) = self.conductor_root_path.parent()
+        if let Some(parent) = self.config.conductor_root_path.parent()
             && fs::remove_dir(parent).is_ok()
         {
             log::info!("Successfully cleaned up all conductor directories");
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::tempdir;
+
+    #[test]
+    fn conductor_dir_retained_after_shutdown() {
+        let tmp = tempdir().unwrap();
+        let conductor_root = tmp.path().join("conductor");
+        let metrics_path = tmp.path().join("metrics.influx");
+
+        let mut builder = HolochainConfigBuilder::default();
+        builder
+            .with_conductor_root_path(&conductor_root)
+            .with_admin_port(0)
+            .with_agent_name("test-agent")
+            .with_metrics_path(&metrics_path);
+        let config = builder.build().expect("Failed to build HolochainConfig");
+        let mut runner = HolochainRunner::create(&config).expect("Failed to create runner");
+
+        // Conductor dir created on runner create
+        assert!(conductor_root.exists(),);
+
+        // Shutdown the runner
+        runner.shutdown();
+
+        // Conductor dir still exists
+        assert!(conductor_root.exists());
+
+        // Drop the runner
+        drop(runner);
+
+        // Conductor dir deleted
+        assert!(!conductor_root.exists());
     }
 }
